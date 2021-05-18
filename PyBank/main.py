b8b354dd8cd68fa991bacf8ed5844f81421989ff
ashleigh <ashleighdevito@gmail.com>
2021-05-18 13:37:57 -0400

    bank accurate still towrite

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,39 +3,51 @@ import csv
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-with open(csvpath, 'r') as csvfile:
+def PyFinancial(file):
 
-    financial_data = csv.reader(csvfile, delimiter=',')
+    with open(csvpath, 'r') as csvfile:
 
-    print(financial_data)
+        financial_data = csv.reader(csvfile, delimiter=',')
+        header = next(financial_data)
 
-    header = next(financial_data)
+        max_profit = 0
+        max_loss = 0
+        total_months = 0
+        pl_sum = 0.0
+        pl_start = 0.0
 
-    print(f"Header:  {header}")
+        for row in financial_data:
+            total_months += 1
+            profit_loss = int(row[1])
+            month = str(row[0])
+            pl_sum += profit_loss
 
-    max_profit = 0
-    max_loss = 0
-    total_months = 0
-    plsum = 0.0
+            if profit_loss > max_profit:
+                max_profit = profit_loss
+                max_month = month
+            elif profit_loss < max_loss:
+                max_loss = profit_loss
+                min_month = month
+        
+            if total_months == 1:
+                pl_start = profit_loss
+        
+            pl_end = profit_loss
 
-    for row in financial_data:
-        print(row)
+        pl_change = pl_end - pl_start
+        average_change = round(pl_change / total_months, 2)
 
-        total_months += 1
-        profit_loss = int(row[1])
-        plsum += profit_loss
-        if profit_loss > max_profit:
-            max_profit = profit_loss
-        elif profit_loss < max_loss:
-            max_loss = profit_loss
+        print("Financial Analysis")
+        print("---------------------")
+        print(f"Total Months: {total_months}")
+        print(f"Total: ${int(pl_sum)}")
+        print("Average Change: $" + "{:.2f}".format(round(average_change,2)))
+        print(f"Greatest Increase in Profits: {max_month} (${max_profit})")
+        print(f"Greatest Decrease in Profits: {min_month} (${max_loss})")
 
-    average_change = int(plsum / total_months)
+output_path = os.path.join("analysis", "bankanalysis.txt")
 
-    print("Financial Analysis")
-    print("---------------------")
-    print(f"Total Months: {total_months}")
-    print(f"Total: {int(plsum)}")
-    print(f"Average Change: {average_change}")
-    print(f"Greatest Increase in Profits: {max_profit}")
-    print(f"Greatest Decrease in Profits: {max_loss}")
+output = str(PyFinancial(csvpath))
 
+with open(output_path, 'w') as txtfile:
+    txtfile.write(output)
