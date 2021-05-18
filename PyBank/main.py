0b1b92aefbe359d2e25d7c7ff7f90585031ef5a2
ashleigh <ashleighdevito@gmail.com>
2021-05-18 12:40:02 -0400

    another step

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,46 +7,35 @@ with open(csvpath, 'r') as csvfile:
 
     financial_data = csv.reader(csvfile, delimiter=',')
 
+    print(financial_data)
+
     header = next(financial_data)
 
-    def PyBank_Analysis (financial_data):
-        month = financial_data[0]
-        profit_loss = financial_data[1]
-        max_profit = 0
-        max_loss = 0
-        total_months = 0
-        plsum = 0.0
+    print(f"Header:  {header}")
 
-        for row in financial_data:
-            total_months += 1
-    
-        print(total_months)
+    max_profit = 0
+    max_loss = 0
+    total_months = 0
+    plsum = 0.0
 
-        for row in financial_data:
-            plsum += profit_loss
+    for row in financial_data:
+        print(row)
 
-        print(plsum)
+        total_months += 1
+        profit_loss = int(row[1])
+        plsum += profit_loss
+        if profit_loss > max_profit:
+            max_profit = profit_loss
+        elif profit_loss < max_loss:
+            max_loss = profit_loss
 
-        for row in financial_data:
-            if profit_loss > max_profit:
-                max_profit = profit_loss
+    average_change = int(plsum / total_months)
 
-        print(max_profit)
+    print("Financial Analysis")
+    print("---------------------")
+    print(f"Total Months: {total_months}")
+    print(f"Total: {int(plsum)}")
+    print(f"Average Change: {average_change}")
+    print(f"Greatest Increase in Profits: {max_profit}")
+    print(f"Greatest Decrease in Profits: {max_loss}")
 
-        for row in financial_data:
-            if profit_loss < max_loss:
-                max_loss = profit_loss
-
-        print(max_loss)
-
-        average_change = plsum / total_months
-
-        print("Financial Analysis")
-        print("---------------------")
-        print(f"Total Months: {total_months}")
-        print(f"Average Change: {average_change}")
-        print(f"Greatest Increase in Profits: {max_profit}")
-        print(f"Greatest Decrease in Profits: {max_loss}")
-    
-    
-print(PyBank_Analysis)
