22cbfa86858fa9b11c4ebd01c883b00d506420bc
ashleigh <ashleighdevito@gmail.com>
2021-05-18 14:09:58 -0400

    start on PyPoll

---
+++ PyPoll/main.py
@@ -0,0 +1,21 @@
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+with open(csvpath, 'r') as csvfile:
+
+        poll_data = csv.reader(csvfile, delimiter=',')
+        header = next(poll_data)
+
+        total_votes = 0
+        candidate_dic = {}
+
+        for row in poll_data:
+            candidate = row[2]
+            total_votes += 1
+            
+            candidate_dic[str(candidate)] = 1
+            
+        print(total_votes)
+        print(candidate_dic)
