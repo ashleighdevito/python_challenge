8786ce1ab1251e09b440bd4bd9b4c08ed55674c7
ashleigh <ashleighdevito@gmail.com>
2021-05-18 23:21:20 -0400

    got the numbers

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,10 +14,29 @@ with open(csvpath, 'r') as csvfile:
         for row in poll_data:
             candidate = row[2]
             total_votes += 1
-            
             candidate_dic[str(candidate)] = candidate_dic.get(str(candidate), 0) + 1
 
         print(total_votes)
 
-        
+        candidate_list = list(candidate_dic)
+        vote_list = list(candidate_dic.values())
+        percents_list = ["%.3f"% ((votes / total_votes) * 100) for votes in vote_list]
+
+        print(candidate_list)
+        print(vote_list)
+        print(percents_list)
         print(candidate_dic)
+
+        print("Election Results")
+        print("---------------------")
+        print(f"Total Votes: {total_votes}")
+        print("---------------------")
+
+        print("---------------------")
+        print(f"Winner: {victor}")
+
+
+
+
+
+
