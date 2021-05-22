89d3c52900e587940f554deb8a7772987b8fe91f
ashleigh <ashleighdevito@gmail.com>
2021-05-21 20:07:23 -0400

    final clean with comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,27 +1,48 @@
+#import dependacies
 import os
 import csv
 
+#define import path for data
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
+#define the function to perform the analysis
 def PyBank():
 
+    #open the data file
     with open(csvpath, 'r') as csvfile:
 
+        #define variable to store data, specify delimiter
         financial_data = csv.reader(csvfile, delimiter=',')
+
+        #assign first row of data as a header
         header = next(financial_data)
 
+        #initialize variables to store 
+        #   greatest increase
+        #   greatest decrease
+        #   total months
+        #   running sum of profit/loss
+        #   starting profit/loss
         max_profit = 0
         max_loss = 0
         total_months = 0
         pl_sum = 0.0
         pl_start = 0.0
 
+        #read each row of data
         for row in financial_data:
+            #add each row to month count
             total_months += 1
-            profit_loss = int(row[1])
+
+            #store current row of data as current month and profit/loss
             month = str(row[0])
+            profit_loss = int(row[1])
+            
+            #running sum of profit/loss
             pl_sum += profit_loss
 
+            #compare stored variables for greatest increase and decrease to current row
+            #   if greater value, store new value and month
             if profit_loss > max_profit:
                 max_profit = profit_loss
                 max_month = month
@@ -29,23 +50,21 @@ def PyBank():
                 max_loss = profit_loss
                 min_month = month
         
+            #store starting value of profit/loss
             if total_months == 1:
                 pl_start = profit_loss
         
+            #store ending value of profit/loss
             pl_end = profit_loss
 
+        #calculate change in profit/loss over the time period
         pl_change = pl_end - pl_start
         average_change = (pl_change / total_months)
+        
+        #format average change and store as string
         average_delta = ("%.2f" % average_change)
 
-        print("Financial Analysis")
-        print("------------------------------")
-        print(f"Total Months: {total_months}")
-        print(f"Total: ${int(pl_sum)}")
-        print(f"Average Change: $ {average_delta}")
-        print(f"Greatest Increase in Profits: {max_month} (${max_profit})")
-        print(f"Greatest Decrease in Profits: {min_month} (${max_loss})")
-
+        #define a variable to output
         output = (
         "Financial Analysis\n"
         "------------------------------\n"
@@ -56,10 +75,17 @@ def PyBank():
         f"Greatest Decrease in Profits: {min_month} (${max_loss})\n"
         )
 
+        #
         return output
 
+#print output to terminal
+analysis = PyBank()
+print(analysis)
+
+#identify path to store output text file
 output_path = os.path.join("analysis", "bank_analysis.txt")
 
+#print output of function into output file
 with open(output_path, 'w') as txtfile:
     analysis = PyBank()
     txtfile.writelines(analysis)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,28 +1,50 @@
+#import dependancies
 import os
 import csv
 
+#define import path for data
 csvpath = os.path.join('Resources', 'election_data.csv')
 
+#open the datafile
 with open(csvpath, 'r') as csvfile:
 
+    #definte a variable for the data
     poll_data = csv.reader(csvfile, delimiter=',')
+    
+    #assign first row of data as a header
     header = next(poll_data)
 
+    #initialize the variable to count the votes
     total_votes = 0
+
+    #create a dictionary to store the named candidates
     candidate_dic = {}
 
+    #read through csv 
     for row in poll_data:
+        #identify vote to which candidate
         candidate = row[2]
+
+        #counting total votes
         total_votes += 1
+
+        #for each vote, add new candidate to dictionary or add vote to current cantidate tally
         candidate_dic[str(candidate)] = candidate_dic.get(str(candidate), 0) + 1
 
+    #define lists to store candidates, how many respective votes
     candidate_list = list(candidate_dic)
     vote_list = list(candidate_dic.values())
+
+    #define a list of candidates percentage of total votes
     percents_list = ["{:.3%}".format(votes / total_votes) for votes in vote_list]
+
+    #define winner as the candidate with most number of votes
     victor = max(candidate_dic, key = lambda key: candidate_dic[key])
 
+    #zip together lists of candidates, their vote percentage, and vote count
     result_summary = zip(candidate_list, percents_list, vote_list)
-        
+    
+    #print analysis to terminal
     print("Election Results")
     print("------------------------------")
     print(f"Total Votes: {total_votes}")
@@ -31,22 +53,28 @@ with open(csvpath, 'r') as csvfile:
         print(f'{cl}: {pl} ({vl})')
     print("------------------------------")
     print(f"Winner: {victor}")
-        
+
+#identify where to open a results file
 output_path = os.path.join("analysis", "election_analysis.txt")
     
+#open the file of where to print output
 with open(output_path, 'w') as txtfile:
     
+    #zip together the three lists defined above
+    result_summary = zip(candidate_list, percents_list, vote_list)
+
+    #print the header and number of total votes as text
     txtfile.write(
         "Election Results\n"
         "------------------------------\n"
         f"Total Votes: {total_votes}\n"
         "------------------------------\n")
 
-    result_summary = zip(candidate_list, percents_list, vote_list)
-    
+    #print results for each candidate
     for cl,pl,vl in result_summary:
         txtfile.write(f'{cl}: {pl} ({vl})\n')
-        
+
+    #print the winning candidate    
     txtfile.write(
         "------------------------------\n"
         f"Winner: {victor}\n")
